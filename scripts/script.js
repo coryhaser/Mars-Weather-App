36eaa6718391aabf3c7b488010b5fab6175d944b
Dan Chovanes <chovanesdan@gmail.com>
2020-12-12 22:26:08 -0500

    tweaking

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,6 @@ const userAction = async () => {
     const response = await fetch('https://api.nasa.gov/insight_weather/?api_key=XQsdsJWF7an4Pge3Rp6qIEcX35IdxI8jfnmSdDWF&feedtype=json&ver=1.0');
     const myJson = await response.json(); //extract JSON from the http response
     // do something with myJson
-
-    console.log(myJson);
   }
+
+document.getElementById("heading").innerHTML = userAction();
