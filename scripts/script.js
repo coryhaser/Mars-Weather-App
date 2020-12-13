cdb226a13bbc49639dbe5e41a166c2a9b3ebdac3
Cory Haser <cory@centsability.io>
2020-12-12 22:35:12 -0500

    Dan help lol

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const {nasa} = require("./config.js")
+import { nasa } from "./config.js";
 
 const userAction = async () => {
     const response = await fetch(`${nasa.api_url}/insight_weather/?api_key=${nasa.api_key}&feedtype=json&ver=1.0`);
@@ -9,5 +9,8 @@ const userAction = async () => {
   }
 
   const getAPOD = async () => {
-    const APOD_response = await fetch(`${nasa.api_url}`)
+    const APOD_response = await fetch(`${nasa.api_url}/planetary/?apodapi_key=${nasa.api_key}`);
+    const APOD_responseJSON = await APOD_response.json();
+
+    console.log(APOD_responseJSON)
   }
