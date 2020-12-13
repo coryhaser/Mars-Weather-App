3897d0283e30857aab4993d53bfd99ad438d23a4
Dan Chovanes <chovanesdan@gmail.com>
2020-12-12 22:51:17 -0500

    defuck

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,16 @@
-const {nasa} = require("./config.js")
+import { nasa } from "./config.js";
 
 const userAction = async () => {
-    const response = await fetch(`${nasa.api_url}/insight_weather/?api_key=${nasa.api_key}&feedtype=json&ver=1.0`);
-    const myJson = await response.json(); //extract JSON from the http response
-    // do something with myJson
-  }
+const response = await fetch(${nasa.api_url}/insight_weather/?api_key=${nasa.api_key}&feedtype=json&ver=1.0);
+const myJson = await response.json(); //extract JSON from the http response
+// do something with myJson
 
-<<<<<<< HEAD
-document.getElementById("heading").innerHTML = userAction();
-=======
-    console.log(myJson);
-  }
+console.log(myJson);
+}
 
-  const getAPOD = async () => {
-<<<<<<< HEAD
-    
-  }
->>>>>>> 35b8997a2ab487c12e040a2a8f1d211c590e7691
-=======
-    const APOD_response = await fetch(`${nasa.api_url}`)
-  }
->>>>>>> 1a5dfdc8223f758ed9071368993f37a21b10f0e8
+const getAPOD = async () => {
+const APOD_response = await fetch(${nasa.api_url}/planetary/?apodapi_key=${nasa.api_key});
+const APOD_responseJSON = await APOD_response.json();
+
+console.log(APOD_responseJSON)
+}
