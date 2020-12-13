35b8997a2ab487c12e040a2a8f1d211c590e7691
Cory Haser <cory@centsability.io>
2020-12-12 22:23:36 -0500

    Added config file

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,13 @@
+const {nasa} = require("./config.js")
+
 const userAction = async () => {
-    const key = 'XQsdsJWF7an4Pge3Rp6qIEcX35IdxI8jfnmSdDWF'
-    const response = await fetch('https://api.nasa.gov/insight_weather/?api_key=XQsdsJWF7an4Pge3Rp6qIEcX35IdxI8jfnmSdDWF&feedtype=json&ver=1.0');
+    const response = await fetch(`https://api.nasa.gov/insight_weather/?api_key=${nasa.api_key}&feedtype=json&ver=1.0`);
     const myJson = await response.json(); //extract JSON from the http response
     // do something with myJson
 
     console.log(myJson);
+  }
+
+  const getAPOD = async () => {
+    
   }
